f0a7e65f98dbd369d06575310bce16a38ef57e73
Leon L. Robinson <leon.robinson@qa.com>
2022-01-24 12:37:57 +0000

    data types

---
+++ data-types.py
@@ -0,0 +1,28 @@
+var1 = "leon"
+var1a = 'leon'
+var2 = 50.0
+var2a = 51.0
+# var3 = leon
+var4 = int(var2)
+var5 = str(var2)
+# concatenate
+print(var1 + var1a)
+# sum
+print(var2 + var2a)
+# concat with data type change
+print(var1 + str(var2))
+# preformatted concat
+print(var1 , var2)
+# F-String
+print(f"I like  {var1}{var2}")
+
+# Escaping
+var6 = "Hello My name is \"Leon\""
+print(var6)
+
+var6a = "Hello My name is \"Leon"
+
+var7 = "I want text \\n accross two lines"
+print(var7)
+
+
